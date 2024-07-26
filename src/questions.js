7aee72e16309d7e3abc00fb5854083352786f0d7
Madge Florina Pereira <madgepereira020701@gmail.com>
2024-07-26 19:50:00 +0530

    added src folder

---
+++ src/questions.js
@@ -0,0 +1,109 @@
+export default [
+  {
+    id: 'q1',
+    text: 'What is the capital city of Australia?',
+    answers: [
+      'Canberra',
+      'Melbourme',
+      'Sydney',
+      'Brisbane',
+    ],
+  },
+  {
+    id: 'q2',
+    text: 'Which river is the longest river in the world?',
+    answers: [
+      'Nile',
+      'Amazon',
+      'Yangtze',
+      'Ganga',
+    ],
+  },
+  {
+    id: 'q3',
+    text: 'Who played the character of Harry Potter in the film series?',
+    answers: [
+      'Daniel Radcliffe',
+      'Rupert Grint',
+      'Elijah Wood',
+      'Tom Felton',
+    ],
+  },
+  {
+    id: 'q4',
+    text: 'How many floors does the Burj Khalifa, the tallest building in the world, have?',
+    answers: [
+      '163',
+      '148',
+      '154',
+      '160',
+    ],
+  },
+  {
+    id: 'q5',
+    text: 'Which Queen song features the iconic lyrics, "Is this the real life? Is this just fantasy?"',
+    answers: [
+      'Bohemian Rhapsody',
+      'We Will Rock You',
+      'Somebody to Love',
+      'Nothing else Matters',
+    ],
+  },
+  {
+    id: 'q6',
+    text: 'As of 2024, which song holds the record for the most views on YouTube?',
+    answers: [
+      '"Despacito" by Luis Fonsi ft. Daddy Yankee',
+      '"Baby Shark Dance" by Pinkfong Kids',
+      'Shape of You" by Ed Sheeran',
+      '"See You Again" by Wiz Khalifa ft. Charlie Puth',
+    ],
+  },
+  {
+    id: 'q7',
+    text: 'Which country won the FIFA World Cup in 2018?',
+    answers: [
+      'Germany',
+      'Brazil',
+      'France',
+      'Argentina',
+    ],
+  },
+
+
+  {
+    id: 'q8',
+    text: '  Who is credited with inventing the World Wide Web?',
+    answers: [
+      'Tim Berners-Lee',
+      'Bill Gates',
+      'Steve Jobs',
+      'Larry Page',
+    ],
+  },
+
+  {
+    id: 'q9',
+    text:'  Which country is known as the Land of the Rising Sun?',
+    answers: [
+      'Japan',
+      'China',
+      'South Korea',
+      'Thailand',
+    ]
+  },
+ 
+  {
+    id: 'q10',
+    text:'Which foreign-language film won the Best Picture Oscar in 2020?',
+    answers: [
+      'Parasite',
+      'Roma',
+      'Amour',
+    'The Lives of Others',
+    ]
+  },
+
+
+  
+];
